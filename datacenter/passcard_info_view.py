424df4de34a7e5ed0cdf80db041ada9b8b75b030
inf <manskiy2k@gmail.com>
2024-08-13 14:15:40 +0300

    #5-7  удалил лишний код #9 добавил в project/passcard_info_view.py from django.shortcuts import get_object_or_404 #13 добавил get_object_or_404 для класса passcard #16 удалил try за ненадобностью #17 строчку перенёс на #22 для удобства чтения кода #43-46 удалил лишний код

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,43 +2,36 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils import timezone
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.exceptions import MultipleObjectsReturned
-from django.http import Http404
 from datacenter.modules import get_duration
 from datacenter.modules import convert_to_moscow_time
 from datacenter.modules import format_duration
 from datacenter.modules import is_visit_long
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    try:
-        passcard = Passcard.objects.get(passcode=passcode, is_active=True)
-        visits = Visit.objects.filter(passcard=passcard)
-        this_passcard_visits = []
-        for visit in visits:
-            entered_at_moscow_time = convert_to_moscow_time(visit.entered_at)
-            if not visit.leaved_at:
-                leaved_at_moscow_time = convert_to_moscow_time(timezone.localtime())
-            else:
-                leaved_at_moscow_time = convert_to_moscow_time(visit.leaved_at)
-            total_duration = get_duration(entered_at_moscow_time, leaved_at_moscow_time)
-            duration = format_duration(total_duration)
-            is_strange = is_visit_long(total_duration)
-            passcard_visits = [
-                {
-                    'entered_at': visit.entered_at,
-                    'duration': duration,
-                    'is_strange': is_strange
-                },
-            ]
-            this_passcard_visits.extend(passcard_visits)
-        context = {
-            'passcard': passcard,
-            'this_passcard_visits': this_passcard_visits
-        }
-        return render(request, 'passcard_info.html', context)
-    except ObjectDoesNotExist:
-        raise Http404("Пропуск с таким кодом не найден")
-    except MultipleObjectsReturned:
-        raise Http404("Найдено несколько пропусков с таким кодом")
+    passcard = get_object_or_404(Passcard, passcode=passcode, is_active=True)
+    visits = Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = []
+    for visit in visits:
+        if not visit.leaved_at:
+            leaved_at_moscow_time = convert_to_moscow_time(timezone.localtime())
+        else:
+            leaved_at_moscow_time = convert_to_moscow_time(visit.leaved_at)
+        entered_at_moscow_time = convert_to_moscow_time(visit.entered_at)
+        total_duration = get_duration(entered_at_moscow_time, leaved_at_moscow_time)
+        duration = format_duration(total_duration)
+        is_strange = is_visit_long(total_duration)
+        passcard_visits = [
+            {
+                'entered_at': visit.entered_at,
+                'duration': duration,
+                'is_strange': is_strange
+            },
+        ]
+        this_passcard_visits.extend(passcard_visits)
+    context = {
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
+    }
+    return render(request, 'passcard_info.html', context)
